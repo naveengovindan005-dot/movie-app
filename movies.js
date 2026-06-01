88b86d24e5d49d9675ff4c931520b3288a5332aa
naveengovindan005-dot <naveen.govindan005@gmail.com>
2026-06-01 13:45:11 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,36 @@
+const movies = [
+  {
+    id: 1,
+    title: "Leo",
+    year: 2023,
+    director: "Lokesh Kanagaraj",
+    genre: "Action",
+    image: "/images/leo.jpg"
+  },
+  {
+    id: 2,
+    title: "Vikram",
+    year: 2022,
+    director: "Lokesh Kanagaraj",
+    genre: "Action Thriller",
+    image: "/images/vikram.jpg"
+  },
+  {
+  id: 3,
+  title: "Sita Ramam",
+  year: 2022,
+  director: "Hanu Raghavapudi",
+  genre: "Romantic Drama",
+  image: "/images/sitaramam.jpg"
+},
+{
+  id: 4,
+  title: "Love Today",
+  year: 2022,
+  director: "Pradeep Ranganathan",
+  genre: "Romantic Comedy",
+  image: "/images/lovetoday.jpg"
+}
+];
+
+module.exports = movies;
